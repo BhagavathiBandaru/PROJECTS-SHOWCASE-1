4f6852e8973e32a5a0e86ec273898aeedc8dcfe8
Bhagavathi Bandaru <107346690+BhagavathiBandaru@users.noreply.github.com>
2022-10-10 12:50:35 +0530

    App.js

---
+++ App.js
@@ -0,0 +1,16 @@
+import ProjectShowcase from './components/ProjectShowcase'
+
+import './App.css'
+
+const categoriesList = [
+  {id: 'ALL', displayText: 'All'},
+  {id: 'STATIC', displayText: 'Static'},
+  {id: 'RESPONSIVE', displayText: 'Responsive'},
+  {id: 'DYNAMIC', displayText: 'Dynamic'},
+  {id: 'REACT', displayText: 'React'},
+]
+console.log(categoriesList)
+
+const App = () => <ProjectShowcase />
+
+export default App
